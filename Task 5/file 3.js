dec580be69a74b0ba33edbf61d20da26f3234a53
Praveen_Kaniki <147649317+PraveenPrabha123@users.noreply.github.com>
2023-11-18 12:21:38 +0530

    Add files via upload

---
+++ Task 5/file 3.js
@@ -0,0 +1,15 @@
+window.alert("welcome");
+console.log("hello world");
+
+var firstNumber= 21;   //variables
+var secondNumber= 32;  
+
+age_of_a_person=18;
+let name="bhavani"
+
+
+result = firstNumber+secondNumber;
+
+console.log(result);
+console.log(age_of_a_person);
+console.log(name);
